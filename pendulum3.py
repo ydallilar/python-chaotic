cf545479f17a65e160de90c22ec6e0e63838a0a4
Yigit Dallılar <psyigit@mintyigit.(none)>
2013-01-29 21:46:48 +0200

    Tue Jan 29 21:46:48 EET 2013

--- pendulum3.py
+++ pendulum3.py
@@ -1,3 +1,4 @@
+##################################################################
 #!/usr/bin/python
 
 #chaotic pendulum problem
@@ -11,12 +12,12 @@
 # q  : damping factor
 # g  : driven force amplitude
 
-
 from numpy import sin,cos,pi
 import matplotlib 
 matplotlib.use("gtk3cairo")
 import matplotlib.pyplot as plt
 
+##################################################################
 # dw/dt
 def fw (t,w,th,ph,q,g,wd) :
   return -w/q-sin(th)+g*cos(ph)
@@ -29,26 +30,38 @@ def fth (t,w,th,ph,q,g,wd) :
 def fph (t,w,th,ph,q,g,wd) :
   return wd
 
+##################################################################
 # just runge-kutta for this problem  
 def solve (init) :
   f = (fw,fth,fph)
   dt = 0.01
   t = 0
   i = 0
+  steps = 2000
   
   res = ([init[0]],[init[1]],[init[2]],init[3],init[4],init[5])
   
-  while (i < 5000) :
+  while (i < steps) :
     j = 0
     while (j < 3) :
       k1 = dt * f[j](t,res[0][i],res[1][i],res[2][i],init[3],init[4],init[5])
       k2 = dt * f[j](t+dt/2,res[0][i]+k1/2,res[1][i]+k1/2,res[2][i]+k1/2,init[3],init[4],init[5])
       k3 = dt * f[j](t+dt/2,res[0][i]+k2/2,res[1][i]+k2/2,res[2][i]+k1/2,init[3],init[4],init[5])
       k4 = dt * f[j](t+dt,res[0][i]+k3,res[1][i]+k3,res[2][i]+k1/2,init[3],init[4],init[5])
-      res[j].append(res[j][i] + 1./6. * (k1 + 2*k2 + 2*k3 + k4))
+      res[j].append((res[j][i] + 1./6. * (k1 + 2*k2 + 2*k3 + k4)))
       j = j + 1
     i = i + 1  
     t = t + dt
   
-  plt.plot(res[0],res[1])
+  i = 0
+  while (i < steps + 1) :
+    if ((res[1][i] % (2*pi)) > (res[1][i] % pi)) : 
+      res[1][i] = - (pi - (res[1][i] % pi))
+    else :
+      res[1][i] = res[1][i] % pi
+    i = i + 1 
+  
+  plt.plot(res[1],res[0],'*')
   plt.show()
+
+##################################################################
