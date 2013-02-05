04043c29b8e66b9312ff7c08a813c61db3b19ed9
pssncp142 <ydallilar@sabanciuniv.edu>
2013-02-05 17:47:43 +0200

    lyapunov exponents complete...

--- logisticmap.py
+++ logisticmap.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 
 ##################################################################
-#  Yigit Dallilar  04.02.2013
-#
-#  Logistic map study libraries for chaotic dynamics.
+#  Yigit Dallilar  04.02.2013                                    #
+#                                                                #
+#  Logistic map study libraries for chaotic dynamics.            #
 ##################################################################
 
 import pylab as plt
 from numpy import linspace
+from sympy import diff,var,log,Abs,N
 
 ##################################################################
 # function with parameters
@@ -37,3 +38,26 @@ def plot(mu,xi,times,steps=50) :
   plt.plot(sp,f(mu,sp,times),'b-')
   plt.plot([0,1],[0,1],'r-')
   plt.show()
+
+##################################################################
+def lyapunovexp(muint,xi,int=0.1,n=1000) :
+  x = var('x')
+  lexp = ([])
+  steps = (muint[1]-muint[0])/int
+  mu = linspace(muint[0],muint[1],num=steps+2)
+  j = 0
+  while ( j < len(mu)) :
+    fp = diff(f(mu[j],x,1),x,1)
+    total = 0.
+    i = 0
+    while (i < n) :
+      total = total + N(log(Abs(fp.subs(x,xi)))) 
+      xi = f(mu[j],xi,1)
+      i = i + 1
+    lexp.append(1./n*total)  
+    j = j + 1
+  
+  plt.plot(mu,lexp,'k-')
+  plt.show()
+  
+##################################################################  
